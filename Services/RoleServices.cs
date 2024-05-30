71e29ba669b942373c17c47f535e369bc2e17831
DarkEmperor16 <vuthse183439@fpt.edu.vn>
2024-05-30 21:58:46 +0700

    Add files via upload

---
+++ Services/RoleServices.cs
@@ -0,0 +1,34 @@
+﻿using BookingV2.Repositories.Entities;
+using BookingV2.Repositories.GenericServices;
+using BookingV2.Repositories.Repo;
+using BookingV2.Repositories.Rsp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class RoleServices : GenericServices<RoleRepo, Role>
+    {
+        private RoleRepo _repo;
+        public RoleServices() 
+        {
+            _repo = new RoleRepo();
+        }
+        public override SingleRsp Read(int id)
+        {
+            var res = new SingleRsp();
+            res.Data = _rep.Read(id);
+            return res;
+        }
+        public MultipleRsp ReadAll()
+        {
+            var res = new MultipleRsp();
+            var roles = _repo.ReadAll();
+            res.SetData("roles", roles);
+            return res;
+        }
+    }
+}
